92d051b7adcfcf34a3d792d8071abc025132adf7
anilkumar8070 <anilprajapati8070@gmail.com>
2025-06-01 23:15:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#3b82f6',
+                        secondary: '#1e40af',
+                        dark: '#1e293b',
+                        light: '#f8fafc',
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                }
+            }
+        }
